b27c93faf22c2463d3ada9e285eb91bbc002316d
Adson <adson.matheus62@gmail.com>
2022-01-19 14:12:07 -0300

    pagina da receita 02

---
+++ pages/novaPagina.js
@@ -0,0 +1,18 @@
+//receita02
+//https://sites.google.com/view/fabricio10/página-inicial/cursos/pweb/receita2
+
+export default function Principal(){
+    return (
+        <div>
+            <h1>Nova Página da Receita 02!</h1>
+            <MariaPrea/>
+        </div>
+    )
+}
+
+//componente
+export function MariaPrea(){
+    return (
+        <h2>Morreu Maria Preá</h2>
+    )
+}
